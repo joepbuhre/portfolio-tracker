45eccb6bb3e63cc41aa759ebf6c16aa5832b845b
Joep Buhre <jcbuhre@gmail.com>
2023-12-19 23:44:41 +0100

    Worked on appmanager

--- backend/app_manager/app_manager.py
+++ backend/app_manager/app_manager.py
@@ -3,6 +3,7 @@ from uuid import uuid4
 import sqlalchemy as sa
 from db_structure import get_db
 from db_structure.sql_meta import StockMeta
+from sqlalchemy.orm import Session
 
 class AppManager:
     def __init__(self) -> None:
@@ -13,18 +14,18 @@ class AppManager:
         newrow = self.meta.users.insert()
         newrow = newrow.values(id=uuid)
 
-        with self.db.connect() as conn:
-            if conn.execute(self.meta.users.select().where(self.meta.users.c.id == uuid)).fetchone() == None:
-                res = conn.execute(newrow)
-                conn.commit()
+        with self.db.connect() as sess:
+            if sess.execute(self.meta.users.select().where(self.meta.users.c.id == uuid)).fetchone() == None:
+                res = sess.execute(newrow)
+                sess.commit()
         
         return uuid
     
     def login(self, uuid: str) -> bool:
         meta = self.meta
         findrow = meta.users.select().where(meta.users.c.id == uuid)
-        with self.db.connect() as conn:
-            res = conn.execute(findrow)
+        with self.db.connect() as sess:
+            res = sess.execute(findrow)
             try:
                 return type(res.fetchone()[0]) == str
             except:
