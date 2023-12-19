e980e1fc33f6280259569f391903494a1dadb574
Joep Buhre <jcbuhre@gmail.com>
2023-12-19 23:43:19 +0100

    Add stock router

--- backend/app/routers/stocks.py
+++ backend/app/routers/stocks.py
@@ -3,9 +3,13 @@ from fastapi import APIRouter, Depends
 from fastapi.responses import PlainTextResponse
 import pandas as pd
 
-from app.dependencies import get_current_user
+from app.dependencies import get_current_user, is_owner
+from stock_importer import StockImporter
 
 from stock_manager import StockManager
+from tomlkit import boolean
+
+from app.types.stocks import StockHistoryBody
 
 router = APIRouter()
 
@@ -21,6 +25,11 @@ def get_history(userid: Annotated[str, Depends(get_current_user)]):
 
     return hist
 
+
+@router.post('/stocks/set-stock-history')
+def stocks_set_stock_history(body: StockHistoryBody,isowner: Annotated[bool, Depends(is_owner)]):
+    return StockImporter().set_history(ticker=body.ticker, filter=dict(body.filter), save=body.save)
+
 @router.get('/stocks/{groupby}')
 def stocks_groupby(user: Annotated[str, Depends(get_current_user)], groupby: Union[str, None] = None):
     sm = StockManager(user)
@@ -32,3 +41,8 @@ def stocks_groupby(user: Annotated[str, Depends(get_current_user)], groupby: Uni
     
     df: pd.DataFrame = sm.get_stocks(groupby)
     return df.to_dict(orient='records')
+
+@router.get('/stocks/statistics')
+def stocks_statistics(user: Annotated[str, Depends(get_current_user)]):
+    sm = StockManager(user)
+
